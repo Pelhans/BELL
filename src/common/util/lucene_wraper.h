caae28841669e2cd1aeffaa49213cd1ef5dbfe24
pelhans <me@pelhans.com>
2022-12-30 17:11:43 +0800

    移除 clucene 中没用的部分

--- src/common/util/lucene_wraper.h
+++ src/common/util/lucene_wraper.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-//#include "clucene_core/CLucene.h"
+#include "clucene_core/src/core/CLucene/analysis/Analyzers.h"
 
 #include <stdio.h>
 #include <iostream>
@@ -12,7 +12,8 @@ using namespace std;
 namespace bell {
 class LuceneWraper {
 public:
-    static void IndexFiles(const char* path, const char* target, const bool clearIndex); 
+    static void IndexFiles(const char* path, const char* target,
+                           const bool clearIndex);
 };
 
-}
+} // namespace bell
--- submodules/clucene_core/src/shared/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pelhans/project/clucene/src/shared
-# Build directory: /Users/pelhans/project/clucene/src/shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
